05ee584c80ae4540f73733a0d0d7b21c5f3d44cc
ciscou <francismpp@gmail.com>
2013-11-09 00:34:31 +0100

    added routing helper railtie

--- lib/temporary_features/railtie.rb
+++ lib/temporary_features/railtie.rb
@@ -1,8 +1,12 @@
 module TemporaryFeatures
   class Railtie < Rails::Railtie
-    initializer "temporary_features.helpers" do
-      ActionController::Base.send :include, Helpers
+    initializer "temporary_features.controller_helpers" do
+      ActionController::Base.send :include, ControllerHelpers
       ActionController::Base.helper_method :temporary_feature
+    end
+
+    initializer "temporary_features.routing_helpers" do
+      ActionDispatch::Routing::Mapper.send :include, RoutingHelpers
     end
   end
 end
